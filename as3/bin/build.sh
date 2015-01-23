4b55eb262c8db4461bb66e87643b46d684139459
katopz <katopz@gmail.com>
2015-01-23 18:11:12 +0700

    add 64-bit support

--- as3/bin/build.sh
+++ as3/bin/build.sh
@@ -1,4 +1,3 @@
 echo "packing ane-device-file-util..."
-cd /Volumes/exFAT/anes/ane-device-file-util/as3/bin
 adt -package -target ane device-file-util.ane extension.xml -swc device-file-util.swc -platform iPhone-ARM library.swf libDeviceFileUtil.a
 echo "Done!"
